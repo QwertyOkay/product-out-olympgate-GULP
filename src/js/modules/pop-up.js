df3cd91dff3cdf26733075583a00f2b11eef6e3d
Alexandr Kotenko <kpshmt@gmail.com>
2023-09-05 17:27:48 +0300

    update

--- src/js/modules/pop-up.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const closeModalBtn = document.querySelector("[data-modal-close7]");
-  const modalBackdrop = document.querySelector("[data-modal7]");
-
-  closeModalBtn.addEventListener("click", (e) => {
-    modalBackdrop.classList.add("is-hidden");
-  });
-})();
